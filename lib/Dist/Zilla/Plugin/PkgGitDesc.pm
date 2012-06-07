f0d205123447f2f27c2df6aec79785312fb5b864
Ricardo Signes <rjbs@cpan.org>
2012-06-07 10:24:26 -0400

    make stuff workier

--- lib/Dist/Zilla/Plugin/PkgGitDesc.pm
+++ lib/Dist/Zilla/Plugin/PkgGitDesc.pm
@@ -1,5 +1,87 @@
-use strict;
-use warnings;
 package Dist::Zilla::Plugin::PkgGitDesc;
+# ABSTRACT: add a $VERSION to your packages
+use Moose;
+with(
+  'Dist::Zilla::Role::FileMunger',
+  'Dist::Zilla::Role::PPI',
+);
 
+use Git::Wrapper;
+use namespace::autoclean;
+
+=head1 SYNOPSIS
+
+in dist.ini
+
+  [PkgGitDesc]
+
+=head1 DESCRIPTION
+
+This plugin will add the long-form git commit description for the current repo
+to the dist's main module as a comment.  It may change, in the future, to put
+things in a package variable, or to provide an option.
+
+It inserts this in the same place that PkgVersion would insert a version.
+
+=cut
+
+sub munge_files {
+  my ($self) = @_;
+
+  my $file = $self->zilla->main_module;
+
+  my $document = $self->ppi_document_for_file($file);
+
+  if ($self->document_assigns_to_variable($document, '$VERSION')) {
+    $self->log([ 'skipping %s: assigns to $VERSION', $file->name ]);
+    return;
+  }
+
+  return unless my $package_stmts = $document->find('PPI::Statement::Package');
+
+  my $git  = Git::Wrapper->new( $self->zilla->root );
+  my $desc = $git->describe({ long => 1 });
+
+  my %seen_pkg;
+
+  for my $stmt (@$package_stmts) {
+    my $package = $stmt->namespace;
+
+    if ($seen_pkg{ $package }++) {
+      $self->log([ 'skipping package re-declaration for %s', $package ]);
+      next;
+    }
+
+    if ($stmt->content =~ /package\s*(?:#.*)?\n\s*\Q$package/) {
+      $self->log([ 'skipping private package %s in %s', $package, $file->name ]);
+      next;
+    }
+
+    my $perl = "# $desc\n";
+
+    my $version_doc = PPI::Document->new(\$perl);
+    my @children = $version_doc->schildren;
+
+    $self->log_debug([
+      'adding git description comment to %s in %s',
+      $package,
+      $file->name,
+    ]);
+
+    Carp::carp("error inserting git description in " . $file->name)
+      unless $stmt->insert_after($children[0]->clone)
+      and    $stmt->insert_after( PPI::Token::Whitespace->new("\n") );
+  }
+
+  $self->save_ppi_document_to_file($document, $file);
+}
+
+__PACKAGE__->meta->make_immutable;
 1;
+__END__
+
+=head1 SEE ALSO
+
+L<PodVersion|Dist::Zilla::Plugin::PkgVersion>
+
+=cut
